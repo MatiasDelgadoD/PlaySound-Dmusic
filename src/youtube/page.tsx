927fb67e029fd63d710247c7e78c85e43fc3dea4
matias delgado <matiasdamian@hotmail.es>
2025-09-14 19:49:26 -0300

    page updatee

--- src/youtube/page.tsx
+++ src/youtube/page.tsx
@@ -3,7 +3,25 @@ import YoutubeSlider from "./youtubeslider"
 
 export default async function YoutubeWindows() {
   const videos = await getYoutubeVideos()
+  const safeVideos = videos?.items ?? []
   return (
-    <YoutubeSlider videos={videos.items} />
+    
+    <YoutubeSlider videos={safeVideos} />
   )
 }
+
+
+// import { getYoutubeVideos } from "./index"
+// import YoutubeSlider from "./youtubeslider"
+
+// export default async function YoutubeWindows() {
+//   const videos = await getYoutubeVideos()
+
+//   // fallback para evitar error
+//   const safeVideos = videos?.items ?? []
+
+//   return (
+//     <YoutubeSlider videos={safeVideos} />
+//   )
+// }
+
